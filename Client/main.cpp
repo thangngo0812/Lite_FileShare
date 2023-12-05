199c935ac3330dcdba849dee0c181ccdf46c6cc3
thangngo0812 <nmquocthang2002@gmail.com>
2023-12-05 09:52:55 +0700

    client send a file and server save this file

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,18 +1,25 @@
-// Client side C/C++ program to demonstrate Socket
-// programming
-#include <arpa/inet.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <unistd.h>
-#define PORT 8080
+#include <arpa/inet.h>
 
-int main(int argc, char const* argv[])
-{
+#define PORT 8081
+
+int main(int argc, char const *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <file_path>\n", argv[0]);
+        return -1;
+    }
+
+    const char *file_path = argv[1];
+
     int status, valread, client_fd;
     struct sockaddr_in serv_addr;
-    char* hello = "Hello from client";
-    char buffer[1024] = { 0 };
+    FILE *file;
+
     if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\n Socket creation error \n");
         return -1;
@@ -21,28 +28,37 @@ int main(int argc, char const* argv[])
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    // Convert IPv4 and IPv6 addresses from text to binary
-    // form
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)
-        <= 0) {
-        printf(
-                "\nInvalid address/ Address not supported \n");
+    if (inet_pton(AF_INET, "10.225.1.98", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
 
-    if ((status
-                 = connect(client_fd, (struct sockaddr*)&serv_addr,
-                           sizeof(serv_addr)))
-        < 0) {
+    if ((status = connect(client_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0) {
         printf("\nConnection Failed \n");
         return -1;
     }
-    send(client_fd, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
-    valread = read(client_fd, buffer, 1024 - 1); // subtract 1 for the null terminator at the end
-    printf("%s\n", buffer);
 
-    // closing the connected socket
+    file = fopen(file_path, "rb");
+    if (file == NULL) {
+        printf("\nError opening file\n");
+        close(client_fd);
+        return -1;
+    }
+
+    // Gửi tên file đến server trước
+    send(client_fd, file_path, strlen(file_path), 0);
+
+    char buffer[1024];
+    size_t bytesRead;
+
+    while ((bytesRead = fread(buffer, 1, sizeof(buffer), file)) > 0) {
+        send(client_fd, buffer, bytesRead, 0);
+    }
+
+    printf("File sent\n");
+
     close(client_fd);
+    fclose(file);
+
     return 0;
 }
--- Server/main.cpp
+++ Server/main.cpp
@@ -1,67 +1,80 @@
-// Server side C/C++ program to demonstrate Socket
-// programming
-#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <unistd.h>
-#define PORT 8080
-int main(int argc, char const* argv[])
-{
+
+#define PORT 8081
+#define BUFFER_SIZE 1024
+#define SAVE_PATH "/root/Desktop/WorkSpace/Lite_FileShare/Server/Saved_file/"
+
+int main(int argc, char const *argv[]) {
     int server_fd, new_socket;
-    ssize_t valread;
     struct sockaddr_in address;
     int opt = 1;
     socklen_t addrlen = sizeof(address);
-    char buffer[1024] = { 0 };
-    char* hello = "Hello from server";
+    char buffer[BUFFER_SIZE] = { 0 };
 
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("socket failed");
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET,
-                   SO_REUSEADDR | SO_REUSEPORT, &opt,
-                   sizeof(opt))) {
-        perror("setsockopt");
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("Setsockopt");
         exit(EXIT_FAILURE);
     }
+
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr*)&address,
-             sizeof(address))
-        < 0) {
-        perror("bind failed");
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
         exit(EXIT_FAILURE);
     }
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    if ((new_socket
-                 = accept(server_fd, (struct sockaddr*)&address,
-                          &addrlen))
-        < 0) {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-    valread = read(new_socket, buffer,
-                   1024 - 1); // subtract 1 for the null
-    // terminator at the end
-    printf("%s\n", buffer);
-    send(new_socket, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
 
-    // closing the connected socket
+    if (listen(server_fd, 3) < 0) {
+        perror("Listen");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen)) < 0) {
+        perror("Accept");
+        exit(EXIT_FAILURE);
+    }
+    char filename_buffer[256];
+    int valread = recv(new_socket, filename_buffer, sizeof(filename_buffer), 0);
+    char *filename = strrchr(filename_buffer, '/');
+    if (filename != NULL) {
+        filename++;
+    } else {
+        filename = filename_buffer;
+    }
+
+    char file_save_path[512];
+    snprintf(file_save_path, sizeof(file_save_path), "%s%s", SAVE_PATH, filename);
+    FILE *received_file = fopen(file_save_path, "wb");
+
+    if (received_file == NULL) {
+        perror("File open failed");
+        exit(EXIT_FAILURE);
+    }
+
+    size_t total_bytes_received = 0;
+    int bytes_received;
+
+    while ((bytes_received = recv(new_socket, buffer, BUFFER_SIZE, 0)) > 0) {
+        total_bytes_received += bytes_received;
+        fwrite(buffer, 1, bytes_received, received_file);
+    }
+
+    fclose(received_file);
+    printf("File received and saved successfully. Total bytes received: %zu\n", total_bytes_received);
+
     close(new_socket);
-    // closing the listening socket
     close(server_fd);
+
     return 0;
 }
